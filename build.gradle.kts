b9b51122336567d7d9e55eb7c5cb7c875fb2a402
Adam Gask <ajgask@gmail.com>
2025-07-31 18:49:16 +0100

    Bump to beta.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     
     // Gr4vy SDK
-    implementation("com.gr4vy:gr4vy-kotlin:1.0.0-beta.1")
+    implementation("com.gr4vy:gr4vy-kotlin:1.0.0-beta.5")
     
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
